9032b557e0f5c5c7489fd82ab8c395bc23139633
hro1025 <hro102025@gmail.com>
2025-11-17 14:11:46 +0100

    new

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Threading.Tasks.Dataflow;
+using Spectre.Console;
+
+namespace kalkulator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Kalkulator calc = new Kalkulator();
+
+        int inputFirstNumberInt;
+        double inputFirstNumberDouble;
+
+        int inputSecondNumberInt;
+        double inputSecondNumberDouble;
+
+        while (true)
+        {
+            Console.Write("Skriv første tall: ");
+            string? inputFirstNumber = Console.ReadLine();
+
+            if (int.TryParse(inputFirstNumber, out inputFirstNumberInt))
+            {
+                break;
+            }
+            if (double.TryParse(inputFirstNumber, out inputFirstNumberDouble))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Bare tall, prøv igjen.");
+            }
+        }
+
+        while (true)
+        {
+            Console.Write("Skriv andre tall: ");
+            string? inputSecondNumber = Console.ReadLine();
+
+            if (int.TryParse(inputSecondNumber, out inputSecondNumberInt))
+            {
+                break;
+            }
+            if (double.TryParse(inputSecondNumber, out inputSecondNumberDouble))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Bare tall, prøv igjen.");
+            }
+        }
+
+        Console.WriteLine("Hva slags formel vil du bruke?\n1:  +\n2:  -\n3:  *");
+
+        while (true)
+        {
+            string? input3 = Console.ReadLine();
+            if (input3 == "1")
+            {
+                Console.WriteLine("1");
+                break;
+            }
+            else if (input3 == "2")
+            {
+                Console.WriteLine("2");
+                break;
+            }
+            else if (input3 == "3")
+            {
+                Console.WriteLine("3");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("velg 1, 2 eller 3");
+            }
+        }
+
+        // Console.WriteLine(calc.AddNumbers(input1, input2));
+    }
+}
---
+++ classes/kalkulator.cs
@@ -0,0 +1,34 @@
+namespace kalkulator;
+
+public class Kalkulator
+{
+    public int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+
+    public double AddNumbers(double a, double b)
+    {
+        return a + b;
+    }
+
+    public int SubtractNumbers(int a, int b)
+    {
+        return a - b;
+    }
+
+    public double SubtractNumbers(double a, double b)
+    {
+        return a - b;
+    }
+
+    public int MultiplyNumber(int a, int b)
+    {
+        return a * b;
+    }
+
+    public double MultiplyNumber(double a, double b)
+    {
+        return a * b;
+    }
+}
